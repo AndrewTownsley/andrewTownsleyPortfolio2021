e24ba73b94c6373a53d3004c484b7a79e85f7259
Andrew TOwnsley <atownsley86@gmail.com>
2021-11-26 17:48:13 -0700

    Mobile menu is working.  I would like to make it so that it closes when  you click anything outside of the menu itself.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const mobileMenu = document.getElementById('mobileMenu');
+const mobileMenuCloseBtn = document.getElementById('mobileMenuCloseBtn');
+const main = document.getElementById('main');
 
 const toggleMobileMenu = () => {
     mobileMenu.classList.toggle('open');
 }
 
+
 mobileMenuBtn.addEventListener('click', toggleMobileMenu);
+mobileMenuCloseBtn.addEventListener('click', toggleMobileMenu);
