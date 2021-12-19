65d01a8df7ea0dca8ae4bbbc7d5e31fffb971d52
Andrew TOwnsley <atownsley86@gmail.com>
2021-12-19 11:28:58 -0700

    Successfully added opacity fade in to content.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const mobileMenu = document.getElementById('mobileMenu');
 const mobileMenuCloseBtn = document.getElementById('mobileMenuCloseBtn');
+const fadeIn = document.querySelectorAll('.fade-in')
 const main = document.getElementById('main');
+
+const options = {
+    root: null, // null == viewport
+    threshold: 0,
+    rootMargin: "-50px"
+ };
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        console.log(entry.target);
+        entry.target.classList.toggle('appear');
+        observer.unobserve(entry.target);
+    })
+}, options);
+
+fadeIn.forEach(section => {
+    observer.observe(section);
+})
 
 const toggleMobileMenu = () => {
     mobileMenu.classList.toggle('open');
